93a29d03572f99e88c461b7efd2bed115b5bb5fd
beabra <beabra@kth.se>
2020-02-09 13:53:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./css/style.css";
+import firebaseConfig from './config/firebase';
+//import * as firebase from 'firebase/app';
 
 function App() {
+
+  const firebase = require('firebase/app');
+  firebase.initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          FLIGHT DATA
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
