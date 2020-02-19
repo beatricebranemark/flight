68ad1ac06b2d6bcd6de7c33463358f819bef86ff
beabra <beabra@kth.se>
2020-02-19 17:54:13 +0100

    more styling made

--- src/components/pages/D3/Map/Map.js
+++ src/components/pages/D3/Map/Map.js
@@ -77,11 +77,11 @@ const Map = props => {
             .attr('fill', function(d) {
               let country = colorMap(d)
 
-              return country.length > 0 ? 'red' : '#b8b8b8'
+              return country.length > 0 ? 'rgba(112,128,144, .2)' : '#b8b8b8'
             }) // //return color(populationById[d.id])
             .attr('d', d3.geoPath().projection(projection))
             .on('mouseover', function(d) {
-              d3.select(this).attr('fill', 'blue')
+              d3.select(this).attr('fill', 'rgba(112,128,144, 1)')
               console.log(d.properties.name)
               let xPosition = d3.mouse(this)[0] - 30
               let yPosition = d3.mouse(this)[1] - 50
@@ -96,7 +96,7 @@ const Map = props => {
               let country = colorMap(d)
               d3.select(this).attr(
                 'fill',
-                country.length > 0 ? 'red' : '#b8b8b8'
+                country.length > 0 ? 'rgba(112,128,144, .2)' : '#b8b8b8'
               )
             })
             .style('stroke', '#fff')
@@ -125,9 +125,9 @@ const Map = props => {
                 .append('path')
                 .attr('d', path(link))
                 .style('fill', 'none')
-                .style('stroke', 'orange')
+                .style('stroke', 'purple')
                 .style('opacity', 0.5)
-                .style('stroke-width', 1)
+                .style('stroke-width', 1.2)
             } catch (TypeError) {
               console.log(TypeError)
             }
@@ -155,7 +155,7 @@ const Map = props => {
         })
     }
   })
-  return <svg width={1100} height={500} ref={d3Container} className="map"></svg>
+  return <svg width={1100} height={700} ref={d3Container} className="map"></svg>
 }
 
 export default Map
