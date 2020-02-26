fad9c66abde778a1db48f8d9bec0e07fb88e829b
Philip <philipax@kth.se>
2020-02-26 10:53:56 +0100

    Sidebar

--- src/components/pages/D3/D3Index.js
+++ src/components/pages/D3/D3Index.js
@@ -7,6 +7,9 @@ import NavBar from './NavBar/NavBar'
 import HomeScreen from './HomeScreen'
 import {Provider} from 'react-redux'
 
+import { useBooleanKnob } from 'retoggle'
+import { Header, Icon, Image,Button, Menu, Segment, Sidebar } from 'semantic-ui-react'
+
 const D3Index = () => {
   const [data, setData] = useState(store.getState().getData.data)
 
@@ -14,26 +17,51 @@ const D3Index = () => {
     setData(store.getState().getData.data)
   })
 
+  function showSideBar() {
+    console.log(visible)
+
+    if (visible == false){
+      
+      setVisible(true)
+    }
+    else {setVisible(false)}
+  }
+  const [visible, setVisible] = useBooleanKnob({ name: 'visible' })
   return (
     <div className='homepage'>
       <div className='homepage__heading'>
         <h1 className='homepage__heading__title'>Flight</h1>
       </div>
-
-      <Provider store={store}>
-        <NavBar data={data} />
-        {data.length == 0 ? (
+        <Provider store={store}>
+          <NavBar data={data} />
+            {data.length == 0 ? (
           <HomeScreen />
-        ) : (
+              ) : (
           <div className='row'>
-            <div className='col-sm-8'>
-              <BarChart />
-              <Map />
-            </div>
-            <div className='col-sm-4'>
+              <Button onClick={showSideBar}>Click Here</Button>
+            <Sidebar.Pushable as={Segment}>
+              <Sidebar
+                as={Menu}
+                animation='push'
+                icon='labeled'
+                inverted
+                onHide={() => setVisible(true)}
+                vertical
+                visible={visible}
+                width='wide'
+              >
+            <Menu.Item>
               <PersonList />
-            </div>
-          </div>
+            </Menu.Item>
+            </Sidebar>
+      <Sidebar.Pusher>
+        <Segment basic>
+        <BarChart />
+              <Map />  
+        </Segment>
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
+  </div>
         )}
       </Provider>
     </div>
