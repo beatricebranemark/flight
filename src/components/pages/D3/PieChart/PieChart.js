ed52474930d629508db41b760c0eaafd44a55415
elinfors <elinfors@kth.se>
2020-03-05 11:20:21 +0100

    pie mouseover

--- src/components/pages/D3/PieChart/PieChart.js
+++ src/components/pages/D3/PieChart/PieChart.js
@@ -1,45 +1,110 @@
 import React, {useEffect, useRef, useState} from 'react'
 import * as d3 from 'd3'
 import './PieChart.css'
-
+import {connect} from 'react-redux'
 
 const PieChart = ({data, filter}) => {
     let organisation_trips = data
+    const d3Container = useRef(null)
+
+    var unique_departure_cities = []
+    var unique_arrival_cities = []
+    var unique_cities = []
+    let table_objects = {}
 
 
-        var width = 450
-        var height = 450
-       var margin = 40
+    // const createArrays = ()=>{
+    organisation_trips.forEach(trip => {
+      var departure = trip.departure_city.split(',')
+      var arrival = trip.arrival_city.split(',')
+      var departure_city = departure[0]
+      var arrival_city = arrival[0]
+      if (unique_departure_cities.includes(departure_city) == false) {
+        unique_departure_cities.push(departure_city)
+      }
+      if (unique_arrival_cities.includes(arrival_city) == false) {
+        unique_arrival_cities.push(arrival_city)
+      }
+    })
+    // }
+
+    // const createUnique = () =>{
+    if (unique_departure_cities.length > unique_arrival_cities) {
+      unique_cities = unique_departure_cities
+      for (var i = 0; i < unique_arrival_cities.length; i++) {
+        if (
+          unique_cities.includes(unique_arrival_cities[i]) == false
+        ) {
+          unique_cities.push(unique_arrival_cities[i])
+        }
+      }
+    } else {
+      for (var i = 0; i < unique_departure_cities.length; i++) {
+        unique_cities = unique_arrival_cities
+
+        if (
+          unique_cities.includes(unique_departure_cities[i]) == false
+        ) {
+          unique_cities.push(unique_departure_cities[i])
+        }
+      }
+    }
+    // }
+
+    const countOccurrances = city => {
+      var count = 0
+      for (var i = 0; i < organisation_trips.length; i++) {
+        if (
+          city == organisation_trips[i].arrival_city.split(',')[0]
+        ) {
+          count += 1
+        }
+      }
+      return count
+    }
+
+    //   const addTableObjects = () =>{
+    unique_cities.forEach(city => {
+      table_objects[city] = countOccurrances(city)
+    })
+    //    }
+
+    console.log(table_objects)
+    var width = 600
+    var height = 600
+    var margin = 40
 
 
     useEffect(()=>{
         // set the dimensions and margins of the graph
-        
+        var chart = d3.select('.pie_chart')
+        chart.remove()
 
         // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
         var radius = Math.min(width, height) / 2 - margin
 
         // append the svg object to the div called 'my_dataviz'
-        var svg = d3.select(".pie_chart")
+        var svg = d3.select(d3Container.current)
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
+        .attr('class', 'pie_chart')
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
         // Create dummy data
-        var data = {a: 9, b: 20, c:30, d:8, e:12, f:3, g:7, h:14}
+        //var data = {a: 9, b: 20, c:30, d:8, e:12, f:3, g:7, h:14}
 
         // set the color scale
         var color = d3.scaleOrdinal()
-        .domain(["a", "b", "c", "d", "e", "f", "g", "h"])
+        //.domain(["a", "b", "c", "d", "e", "f", "g", "h"])
         .range(d3.schemeDark2);
 
         // Compute the position of each group on the pie:
         var pie = d3.pie()
         .sort(null) // Do not sort group by size
         .value(function(d) {return d.value; })
-        var data_ready = pie(d3.entries(data))
+        var data_ready = pie(d3.entries(table_objects))
 
         // The arc generator
         var arc = d3.arc()
@@ -51,6 +116,7 @@ const PieChart = ({data, filter}) => {
         .innerRadius(radius * 0.9)
         .outerRadius(radius * 0.9)
 
+        var divTooltip = d3.select('#PieChartToolTip');
         // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
         svg
         .selectAll('allSlices')
@@ -62,10 +128,20 @@ const PieChart = ({data, filter}) => {
         .attr("stroke", "white")
         .style("stroke-width", "2px")
         .style("opacity", 0.7)
-        .on('mouseover', function(d){console.log(d.data.key)})
+        .on('mouseover', function(d) {
+            d3.select('.mouse_text').remove()
+            svg.append("text")
+            .attr("class", "mouse_text")
+            .attr("text-anchor", "middle")
+            .attr('font-size', '3em')
+            .attr('y', 10)
+            .attr('textLength', radius*0.8)
+            .text(d.data.key+': '+d.data.value);
+          })
 
-        // Add the polylines between chart and labels:
        
+        // Add the polylines between chart and labels:
+      /* 
         svg
         .selectAll('allPolylines')
         .data(data_ready)
@@ -89,27 +165,36 @@ const PieChart = ({data, filter}) => {
         .data(data_ready)
         .enter()
         .append('text')
-        .text( 'tjena' )
+        .text( function(d) { return d.data.key } )
         .attr('class', 'text_pie')
         .attr('transform', function(d) {
             var pos = outerArc.centroid(d);
-            //var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-           // pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
             return 'translate(' + pos + ')';
         })
         .style('text-anchor', function(d) {
             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
             return (midangle < Math.PI ? 'start' : 'end')
         })
-
+*/
 
     })
     return(
         <React.Fragment>
-        <div className="pie_chart"></div>
-        </React.Fragment>
+       <svg width={width} height={height} ref={d3Container}></svg>
+       <div id="PieChartToolTip" style={{position: 'absolute',backgroundColor:'transparent',padding:5+'px',fontSize:12+'px'}}></div>
+       </React.Fragment>
     )
 }
 
-
-export default (PieChart)
+const mapStateToProps = (state, ownProps) => {
+    let newData =
+      state.getMap.data.length == 0 ? state.getData : state.getMap
+    return {
+      data: newData.data,
+      filter: ownProps.filter,
+    }
+  }
+  
+  export default connect(mapStateToProps)(PieChart)
