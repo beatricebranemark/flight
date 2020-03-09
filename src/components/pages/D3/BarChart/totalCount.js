7e013e964572c28281136c5e08b78c1d7f96c332
viktorle <35763150+viktorle@users.noreply.github.com>
2020-03-09 16:07:23 +0100

    småfix

--- src/components/pages/D3/BarChart/totalCount.js
+++ src/components/pages/D3/BarChart/totalCount.js
@@ -1,36 +1,39 @@
 import React from 'react'
-import { Icon, Image, Statistic } from 'semantic-ui-react'
+import {Icon, Image, Statistic} from 'semantic-ui-react'
 import Model from '../../../../data/model'
 import {connect} from 'react-redux'
 
-
-const TotalCounts = (data,props) => {
-
-    return (
-    
+const TotalCounts = (data, props) => {
+  return (
     <>
-  <Statistic.Group>
-  
+      <Statistic.Group>
+        <Statistic>
+          <Statistic.Value>
+            <Icon name='plane' />
+            {data.data.length}
+          </Statistic.Value>
+          <Statistic.Label>Flights</Statistic.Label>
+        </Statistic>
+      </Statistic.Group>
+    </>
+  )
+}
 
-    <Statistic>
-      <Statistic.Value>
-        <Icon name='plane' />{data.data.length}
-      </Statistic.Value>
-      <Statistic.Label>Flights</Statistic.Label>
-    </Statistic>
-
-  </Statistic.Group>
-  </>
-    )
+const mapStateToProps = (state, ownProps) => {
+  let newData
+  if (state.getSelectedOrg.data.length === 0) {
+    if (state.getSelectedSchool.data.length > 0) {
+      newData = state.getSelectedSchool
+    } else {
+      newData = state.getData
     }
+  } else {
+    newData =
+      state.getMap.data.length > 0 ? state.getMap : state.getData
+  }
+  return {
+    data: newData.data,
+  }
+}
 
-    const mapStateToProps = (state, ownProps) => {
-        let newData = state.getMap.data.length == 0 ? state.getData : state.getMap
-        
-        return {
-          data: newData.data,
-        }
-        
-    }
-
-      export default connect(mapStateToProps)(TotalCounts)
+export default connect(mapStateToProps)(TotalCounts)
